d18763624f99c675c449ad0f9d82195f78ca85d1
git <>
2021-06-18 20:11:04 +0300

    init

---
+++ src/components/app/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Avto-Moto</h1>
+  );
+};
+
+export {App};
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, {StrictMode} from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter as Router} from 'react-router-dom';
+
+import {App} from './components/app/app';
+
+import './index.scss';
+
+ReactDOM.render((
+  <StrictMode>
+      <Router>
+        <App/>
+      </Router>
+  </StrictMode>
+), document.querySelector(`#root`));
+
